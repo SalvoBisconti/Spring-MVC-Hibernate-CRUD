a7368580e38d730be52cfa07925b53385c483b3c
Salvatore Bisconti <121030864+SalvoBisconti@users.noreply.github.com>
2024-12-11 19:38:40 +0100

    commit

---
+++ src/main/java/com/advancia/stage/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.advancia.stage.service;
+
+import java.util.List;
+
+import com.advancia.stage.model.Employee;
+
+public interface EmployeeService {
+
+	public void addEmployee(Employee employee);
+
+	public List<Employee> getAllEmployees();
+
+	public void deleteEmployee(Integer employeeId);
+
+	public Employee getEmployee(int employeeid);
+
+	public Employee updateEmployee(Employee employee);
+}
